5b083ae8adc47443d375d53500db62e14313e1b7
creative.darkstar <creative.darkstar@gmail.com>
2024-05-16 14:41:29 +0900

    Add: accounts - login, logout, refresh

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
 
 
 app_name = "accounts"
 
 urlpatterns = [
+    # ---------- API - api ---------- #
+    path("api/login/", TokenObtainPairView.as_view(), name='login'),
+    path("api/refresh/", TokenRefreshView.as_view(), name='refresh_token'),
+    path("api/logout/", TokenBlacklistView.as_view(), name='logout'),
 
+    # ---------- Web - '' ---------- #
 ]
