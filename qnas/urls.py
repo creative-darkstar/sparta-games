06de602054ec6a838f00f7b7a762e3cb0e7030e4
Park SungHyun <sunghyun0620@gmail.com>
2024-05-20 10:05:48 +0900

    Add:qnas GetDetail, PutDetail, DeleteDetail

--- qnas/urls.py
+++ qnas/urls.py
@@ -6,4 +6,5 @@ app_name = "qnas"
 
 urlpatterns = [
     path("api/post/", views.QnAPostListAPIView.as_view(), name="qna_list"),
+    path("api/post/<int:qna_pk>/", views.QnADetailAPIView.as_view(), name="qna_detail"),
 ]
--- qnas/views.py
+++ qnas/views.py
@@ -44,45 +44,44 @@ class QnAPostListAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-# class GameDetailAPIView(APIView):
-#     """
-#     포스트일 때 로그인 인증을 위한 함수
-#     """
-#     def get_permissions(self):  # 로그인 인증토큰
-#         permissions = super().get_permissions()
+class QnADetailAPIView(APIView):
+    """
+    포스트일 때 로그인 인증을 위한 함수
+    """
+    def get_permissions(self):  # 로그인 인증토큰
+        permissions = super().get_permissions()
 
-#         if self.request.method.lower() == ('put'or'delete'):  # 포스트할때만 로그인
-#             permissions.append(IsAuthenticated())
+        if self.request.method.lower() == ('put'or'delete'):  # 포스트할때만 로그인
+            permissions.append(IsAuthenticated())
 
-#         return permissions
+        return permissions
     
-#     def get_object(self, game_pk):
-#         return get_object_or_404(Game, pk=game_pk, is_visible=True)
+    def get_object(self, qna_pk):
+        return get_object_or_404(QnA, pk=qna_pk)
 
-#     """
-#     게임 상세 조회
-#     """
-#     def get(self, request, game_pk):
-#         game=self.get_object(game_pk)
-#         game.view_cnt += 1  # 아티클 뷰수 조회
-#         game.save()  # 아티클 뷰수 조회
-#         serializer = GameDetailSerializer(game)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
+    """
+    게임 상세 조회
+    """
+    def get(self, request, qna_pk):
+        qna=self.get_object(qna_pk)
+        serializer = QnAPostListSerializer(qna)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-#     """
-#     게임 수정
-#     """
-#     def put(self, request, game_pk):
-#         
+    """
+    게임 수정
+    """
+    def put(self, request, qna_pk):
+        qna=self.get_object(qna_pk)
+        serializer = QnAPostListSerializer(
+            qna, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
 
-#     """
-#     게임 삭제
-#     """
-#     def delete(self, request, game_pk):
-#         game = self.get_object(game_pk)
-#         if game.maker == request.user:
-#             game.is_visible=False
-#             game.save()
-#             return Response({"message":"삭제를 완료했습니다"},status=status.HTTP_204_NO_CONTENT)
-#         else:
-#             return Response({"error":"작성자가 아닙니다"},status=status.HTTP_400_BAD_REQUEST)
+    """
+    게임 삭제
+    """
+    def delete(self, request, qna_pk):
+        qna=self.get_object(qna_pk)
+        qna.delete()
+        return Response({"message":"삭제를 완료했습니다"},status=status.HTTP_204_NO_CONTENT)
