7a22119c8d6a3041cea0d78cd30adaf656e8e729
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-05-30 13:12:40 +0900

    Edit: settings.py ALLOWED_HOSTS

--- spartagames/settings.py
+++ spartagames/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = config.DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '13.209.74.174',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
