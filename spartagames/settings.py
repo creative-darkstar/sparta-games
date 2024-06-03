b4644478c8b03ab51d5b82f5aa615fbc1860b520
creative.darkstar <creative.darkstar@gmail.com>
2024-06-03 19:38:18 +0900

    Edit: server setting

--- spartagames/settings.py
+++ spartagames/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '13.209.74.174',
--- spartagames/urls.py
+++ spartagames/urls.py
@@ -30,5 +30,4 @@ urlpatterns = [
     path('search/', views.test_search_view, name='test_search_view'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
