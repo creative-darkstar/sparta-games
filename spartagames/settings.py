8b9c39e726511f82a9473f63577180d460c48121
creative.darkstar <creative.darkstar@gmail.com>
2024-06-05 09:45:35 +0900

    Edit: server setting - using AWS RDS PostgreSQL

--- spartagames/settings.py
+++ spartagames/settings.py
@@ -93,10 +93,20 @@ WSGI_APPLICATION = 'spartagames.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': config.DATABASES["host"],
+        'PORT': config.DATABASES["port"],
+        'NAME': config.DATABASES["database"],
+        'USER': config.DATABASES["user"],
+        'PASSWORD': config.DATABASES["password"],
     }
 }
 
