e16608eff387fc7cf2933f09132e60a5fb360426
Park SungHyun <sunghyun0620@gmail.com>
2024-06-11 20:08:52 +0900

    Add:chatbot Set Limit Chatbot 10 per day

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -9,3 +10,14 @@ class User(AbstractUser):
         blank=True,
         null=True,
     )
+
+class BotCnt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'date')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.date} - {self.count}'
