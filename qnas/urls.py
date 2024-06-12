47ce636051e4b36729ee7fa2b7403fa911c3d21e
hjn5018 <hjn5018@gmail.com>
2024-06-12 12:46:59 +0900

    Edit: test_main_view to main_view & test_search_view to search_view

--- qnas/urls.py
+++ qnas/urls.py
@@ -5,11 +5,12 @@ from . import views
 app_name = "qnas"
 
 urlpatterns = [
+    # ---------- API---------- #
     path("api/post/", views.QnAPostListAPIView.as_view(), name="qna_list"),
     path("api/post/<int:qna_pk>/", views.QnADetailAPIView.as_view(), name="qna_detail"),
     path('api/categories/', views.CategoryListView.as_view(), name='category_list'),
 
-    #Templates
+    # ---------- Web---------- #
     path("", views.qna_main_view, name="qna_main"),
     path("<int:qna_pk>/", views.qna_detail_view, name="qna_detail_page"),
     path("create/", views.qna_create_view, name="qna_create"),
